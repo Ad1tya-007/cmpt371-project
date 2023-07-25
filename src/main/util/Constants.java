350e5eadfa93f2bf1170f634ebb16c51ed227e3b
Ad1tya-007 <adityakul0314@gmail.com>
2023-07-24 23:15:01 -0700

    successfully implemented socket connection

--- src/main/util/Constants.java
+++ src/main/util/Constants.java
@@ -1,9 +1,9 @@
-package util;
+package main.util;
 
 public class Constants {
     public int SCREEN_WIDTH = 600;
     public int SCREEN_HEIGHT = 600;
     public int UNIT_SIZE = 25;
-    public int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    public int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
     public int DELAY = 75;
 }
